dd2e4d31b9f82ce6d7b2d7639290c22f7123d2d9
David <61760404+davidotvos@users.noreply.github.com>
2023-07-18 20:32:29 +0200

    only allowing json files commit

--- Task1_PPTX_report/main.py
+++ Task1_PPTX_report/main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 import json
 import argparse
+import os
 
+# 
+def validate_json_file(file_path):
+    if not file_path.lower().endswith('.json') or not os.path.isfile(file_path):
+        raise argparse.ArgumentTypeError('Invalid JSON file: {}'.format(file_path))
+    return file_path
 
 def main():
     # Instantiate the parser
     parser = argparse.ArgumentParser(description='Generate a pptx report from a configuration file.')
-    parser.add_argument('config_file', help='Path to the JSON configuration file')
+    parser.add_argument('config_file',type=validate_json_file, help='Path to the JSON configuration file')
 
     args = parser.parse_args()
+    config_file = args.config_file
+
+    print(config_file)
 ##############################################################################
 
 if __name__ == "__main__":
